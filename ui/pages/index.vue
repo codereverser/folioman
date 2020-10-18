6b45a0cc74e58c41c01914da5c3f87fb1da9a209
Sandeep Somasekharan <codereverser@gmail.com>
2020-10-18 11:45:05 +0530

    ui skeleton code

---
+++ ui/pages/index.vue
@@ -0,0 +1,48 @@
+<template lang="pug">
+  .container
+    div.card
+      Logo
+      h1.title folioman
+      .links
+        a.button--green(href="https://nuxtjs.org/" target="_blank" rel="noopener noreferrer") Documentation
+        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank" rel="noopener noreferrer") GitHub
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+</script>
+
+<style lang="scss">
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
---
+++ ui/plugins/primevue.js
@@ -0,0 +1,7 @@
+import Vue from 'vue';
+import InputText from 'primevue/inputtext';
+import Button from 'primevue/button';
+
+
+Vue.component('InputText', InputText);
+Vue.component('Button', Button);
