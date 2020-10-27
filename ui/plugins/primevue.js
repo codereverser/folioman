88a5fd2a8339b824997b49055a711b5f175256ec
Sandeep Somasekharan <codereverser@gmail.com>
2020-10-28 00:48:50 +0530

    primevue plugin updated

--- ui/plugins/primevue.js
+++ ui/plugins/primevue.js
@@ -7,7 +7,12 @@ import FileUpload from "primevue/fileupload";
 import InputText from "primevue/inputtext";
 import Panel from "primevue/panel";
 import ProgressBar from "primevue/progressbar";
+import ProgressSpinner from "primevue/progressspinner";
 import Steps from "primevue/steps";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+
+Vue.use(ToastService);
 
 Vue.component("Button", Button);
 Vue.component("Card", Card);
@@ -17,4 +22,6 @@ Vue.component("FileUpload", FileUpload);
 Vue.component("InputText", InputText);
 Vue.component("Panel", Panel);
 Vue.component("ProgressBar", ProgressBar);
+Vue.component("ProgressSpinner", ProgressSpinner);
 Vue.component("Steps", Steps);
+Vue.component("Toast", Toast);
