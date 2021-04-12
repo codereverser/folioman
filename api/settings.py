183c394e29015dfb1756e9841b8f9ad4c350fe30
Sandeep Somasekharan <codereverser@gmail.com>
2021-04-12 20:32:12 +0530

    finalize docker deployment files

--- api/settings.py
+++ api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
 APPEND_SLASH = False
 
 # Application definition
@@ -133,6 +133,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
 
 LOGGING = {
     "version": 1,
