a36d8001e49f579f93a16e7069bf402bac442668
Sandeep Somasekharan <codereverser@gmail.com>
2026-06-09 11:18:57 +1000

    Update frontend dev deps and migrate to @primeuix/themes
    Replace the deprecated @primevue/themes with its successor @primeuix/themes
    (token-compatible with the PrimeVue 4 core), align @types/node with the Node 24
    runtime, and refresh jsdom and @vue/test-utils.

--- frontend/src/theme/folioman-preset.ts
+++ frontend/src/theme/folioman-preset.ts
@@ -1,5 +1,5 @@
-import { definePreset } from '@primevue/themes'
-import Aura from '@primevue/themes/aura'
+import { definePreset } from '@primeuix/themes'
+import Aura from '@primeuix/themes/aura'
 
 /**
  * Folioman theme preset.
